20d20f00e1ec5e16ffca8c005dd6034d3082a2ae
Andy <andrew.sunglae.kim@gmail.com>
2015-01-26 17:04:56 -0500

    communicating with twitter now

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,17 @@
 class StaticPagesController < ApplicationController
-	before_action :authenticate_user!
-	def index
+	before_action :authenticate_user!, :set_twitter_api
+	def index	
+		
 		@questions = Question.all
+
+	end
+	private
+	def set_twitter_api
+		@twitter = Twitter::REST::Client.new do |config|
+		  config.consumer_key = ENV['twitter_consumer_key']
+		  config.consumer_secret = ENV['twitter_consumer_secret_key']
+		  config.access_token = ENV['twitter_access_token']
+		  config.access_token_secret = ENV['twitter_secret_access_token']
+		end
 	end
 end
--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,4 +1,4 @@
-$twitter = Twitter::REST::Client.new do |config|
+twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV['twitter_consumer_key']
   config.consumer_secret = ENV['twitter_consumer_secret_key']
   config.access_token = ENV['twitter_access_token']
