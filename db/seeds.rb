eb27060b50a2dffc4c772acbc678ccbe5913845f
Aurora Nou <auroranou@gmail.com>
2015-01-26 10:49:11 -0500

    add fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,14 @@ Group.destroy_all
 users = User.create([
 	{email: 'bob@email.com', password: 'password'},
 	{email: 'andy@email.com', password: 'password'},
-	{email: 'george@email.com', pasword: 'password'}
+	{email: 'george@email.com', password: 'password'}
 ])
 
 groups = Group.create([
-	{group: 'Team 1'},
-	{group: 'Team 2'}
+	{name: 'Team 1'},
+	{name: 'Team 2'}
 ])
 
-Group.all[0].users << User.first, User.last
+Group.all[0].users << User.first
+Group.all[0].users << User.last
 Group.all[1].users << User.all[1]
