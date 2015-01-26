2227f54c7e46ae042975c8a214f374fa8d4f98f0
Aurora Nou <auroranou@gmail.com>
2015-01-26 10:30:56 -0500

    create join table for users and groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126152215) do
+ActiveRecord::Schema.define(version: 20150126152826) do
 
   create_table "groups", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "groups_users", id: false, force: :cascade do |t|
+    t.integer "user_id",  null: false
+    t.integer "group_id", null: false
   end
 
   create_table "questions", force: :cascade do |t|
