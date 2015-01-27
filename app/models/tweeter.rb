5a16d590f4220792a11c587d928c4fe708c93f72
Aurora Nou <auroranou@gmail.com>
2015-01-27 15:30:32 -0500

    add scheduler rake task and tweeter model methods

--- app/models/tweeter.rb
+++ app/models/tweeter.rb
@@ -1,3 +1,18 @@
 class Tweeter < ActiveRecord::Base
 	validates :name, :username, :follower_count, presence: true
+
+	def update_attributes
+		set_twitter_api
+		self.follower_count = @twitter.followers(self.username).count
+	end
+
+	private
+	def set_twitter_api
+		@twitter = Twitter::REST::Client.new do |config|
+		  config.consumer_key = ENV['twitter_consumer_key']
+		  config.consumer_secret = ENV['twitter_consumer_secret_key']
+		  config.access_token = ENV['twitter_access_token']
+		  config.access_token_secret = ENV['twitter_secret_access_token']
+		end
+	end
 end
