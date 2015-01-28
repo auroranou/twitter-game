9293623f41c8081b793a57ef5064f8b069dd468b
Aurora Nou <auroranou@gmail.com>
2015-01-28 16:03:25 -0500

    update index view to respond to and return json objects

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,15 @@
 class StaticPagesController < ApplicationController
 	before_action :authenticate_user!
-	#before_action :set_twitter_api, only: [:index]
-	def index	
-		@questions = Question.all.sample
-		@tweeters = Tweeter.all.sort_by{rand}
-		@first = @tweeters[0]
-		@second = @tweeters[1]
+
+	def index
+		@random_question = Question.random_question
+		@question = @random_question[:question]
+		@first = @random_question[:first]
+		@second = @random_question[:second]
 		@users = User.all.sort_by{ |user| user.score }.reverse
-		@answer = Answer.last
 		respond_to do |format|
 			format.html
-			format.json {render json: @answer}
+			format.json {render json: @random_question}
 		end
 	end
 
@@ -25,15 +24,6 @@ class StaticPagesController < ApplicationController
 	end
 	
 	private
-	def set_twitter_api
-		@twitter = Twitter::REST::Client.new do |config|
-		  config.consumer_key = ENV['twitter_consumer_key']
-		  config.consumer_secret = ENV['twitter_consumer_secret_key']
-		  config.access_token = ENV['twitter_access_token']
-		  config.access_token_secret = ENV['twitter_secret_access_token']
-		end
-	end
-
 	def redirect_to_index_after_answer
 		if @answer.save
 			redirect_to static_pages_path
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ActiveRecord::Base
 	validates_presence_of :body
 	has_many :answers
 	has_many :users, through: :answers
+
+	def self.random_question
+		question = Question.all.sample
+		tweeters = Tweeter.all.sort_by{rand}
+		first = tweeters[0]
+		second = tweeters[1]
+		return {question: question, first: first, second: second}
+	end
 end
