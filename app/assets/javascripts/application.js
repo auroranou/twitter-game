219c2efa4e5186585d738e27f5272b628dc60b44
Aurora Nou <auroranou@gmail.com>
2015-01-29 17:55:47 -0500

    REFACTOR ALL JS TO OOP (except for scoreboard updating)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,140 +17,142 @@
 
 var click;
 
-function loadQuestion() { 
-	$('.next').remove();
-	$.ajax({
-		type: 'GET', 
-		dataType: 'json', 
-		url: window.location.origin + '/questions.json',
-		success: function(response) {
-			var question = response['question'];
-			var firstAns = response['first'];
-			var secondAns = response['second'];
-			$('.question').attr('id', question['id']);
-			$('.question').html(question['body']);
-			$('.answer:first-child').html(firstAns['name'] + ' (@' + firstAns['username'] + ')');
-			$('.answer:last-child').html(secondAns['name'] + ' (@' + secondAns['username'] + ')');
-			checkAnswer(response);
-		},
-		error: function(response) {
-			console.log('error in loadQuestion ' + response);
-		}
-	});
-	click = 0;
+var Question = function(){
 }
 
-function checkAnswer(response) {
-	var questionParam = response['question']['parameter'];
-	var firstAns = response['first'];
-	var secondAns = response['second'];
-	switch(questionParam) {
-		case 'followers_count':
-			$('.answer:first-child').attr('id', ' has ' + firstAns['followers_count'] + ' followers');
-			$('.answer:last-child').attr('id', ' has ' + secondAns['followers_count'] + ' followers');
-			most();
-			break;
-		case 'friends_count':
-			$('.answer:first-child').attr('id', ' follows ' + firstAns['friends_count']);
-			$('.answer:last-child').attr('id', ' follows ' + secondAns['friends_count']);
-			most();
-			break;
-		case 'statuses_count':
-			$('.answer:first-child').attr('id', ' has ' + firstAns['statuses_count'] + ' tweets');
-			$('.answer:last-child').attr('id', ' has ' + secondAns['statuses_count'] + ' tweets');
-			most();
-			break;
-		case 'creation_date':
-			$('.answer:first-child').attr('id', ' has been on Twitter since ' + firstAns['creation_date']);
-			$('.answer:last-child').attr('id', ' has been on Twitter since ' + secondAns['creation_date']);
-			oldest();
-			break
-		default:
-			console.log('error in checkAnswer!');
-	}
-	// console.log(response);
-}
-
-function most() {
-	$('.answer').on('click', function(event){
-		event.preventDefault();
-		if (click < 1) {
-			click ++;
-			( $(this).attr('id') > $(this).siblings('.answer').attr('id') ) ? createAnswer('correct') : createAnswer('wrong');
-		}
-	});
-}
-
-function oldest() {
-	$(".answer").on('click', function(event){
-		event.preventDefault();
-		if(click < 1) {
-			click++
-			var date1 = new Date($(this).attr('id'));
-			var date2 = new Date($(this).siblings('.answer').attr('id'));
-			( date1.getTime() > date2.getTime() ) ? createAnswer('correct') : createAnswer('wrong');
-	  }
-	});
-}
-
-function createAnswer(attr) {
-	var userId = $('.user').attr('id');
-	var questionId = $('.question').attr('id');
-	var url = window.location.origin + '/questions/' + questionId;
-	var data = {
-		'user_id': userId,
-		'question_id': questionId
-	}
-	if (attr == 'correct') {
-		url += '/create_right_answer';
-		data['is_correct?'] = true
-	}
-	else {
-		url += '/create_wrong_answer';
-		data['is_correct?'] = false
-	}
-	$.ajax({
-		type: 'POST',
-		dataType: 'json',
-		url: url,
-		data: data,
-		success: function(response, status, jqXHR){
-			console.log('answer created!');
-		},
-		error: function(response){
-			console.log('error in createAnswer ' + response);
-		}
-	});
-	$('.question').html(attr + '!');
-	$('.answer').each(function(){
-		$(this).append( $(this).attr('id') );
-	});
-	next();
-}
-
-function next() {
-	$('.quizDialog div').append('<button class="next">Next</button>');
-	$('.next').on('click', function(event){
-		loadQuestion();
-	});
-}
-
-function updateScoreboard(){
-	$(".close").on("click", function(){
+Question.prototype = {
+	load: function(callback){
 		$.ajax({
-			type: "GET",
-			dataType: "json",
-			url: window.location.origin + "/users.json",
-			success: function(response){
-				$(".scoreboard").children().remove()
-				for(var i = 0; i < response.length; i++){
-					$(".scoreboard").append("<li><p>" + response[i]["email"]+ "</p><p>Score: " + response[i]["score"] + "</p><div class='bar-wrap'><span class='bar-fill' style='width: " + (response[i]["score"]/response[0]["score"]) * 100 + "%;'></span></div></li>");
-				}
-			},
-			error: function(response){
-				console.log("error");
+			type: 'GET', 
+			dataType: 'json', 
+			url: window.location.origin + '/questions.json',
+			success: callback
+		});
+	}
+}
+	
+var QuestionView = function(model){
+	this.model = model;
+	this.questionId = model['question']['id']
+	this.questionBody = model['question']['body']
+	this.questionParameter = model['question']['parameter']
+	this.firstName = model['first']['name']
+	this.firstUserName = model['first']['username']
+	this.firstFollowers = model['first']['followers_count']
+	this.firstFriends = model['first']['friends_count']
+	this.firstStatuses = model['first']['statuses_count']
+	this.firstCreationDate = model['first']['creation_date']
+	this.secondName = model['second']['name']
+	this.secondUserName = model['second']['username']
+	this.secondFollowers = model['second']['followers_count']
+	this.secondFriends = model['second']['friends_count']
+	this.secondStatuses = model['second']['statuses_count']
+	this.secondCreationDate = model['second']['creation_date']
+	this.renderQuestion(this.checkAnswer.bind(this));
+}
+
+QuestionView.prototype = {
+	renderQuestion: function(callback){
+		$('.question').html(this.questionBody);
+		$('.answer:first-child').html(this.firstName + ' (@' + this.firstUserName + ')');
+		$('.answer:last-child').html(this.secondName + ' (@' + this.secondUserName + ')');
+		callback(this.questionParameter);
+	},
+	checkAnswer: function(questionParameter){
+		var self = this
+		$('.answer').unbind('click');
+		$('.answer').on('click', function(event){
+			event.preventDefault();
+			click++
+			switch(questionParameter) {
+				case 'followers_count':
+					(self.firstFollowers > self.secondFollowers ? self.firstName : self.secondName);
+					($(this).html().split(" (@")[0] == self.firstName ? self.createAnswer("right") : self.createAnswer("wrong")); 
+					$('.answer:first-child').append(' has ' + self.firstFollowers + ' followers');
+					$('.answer:last-child').append(' has ' + self.secondFollowers + ' followers');
+					break;
+				case 'friends_count':
+					(self.firstFriends > self.secondFriends ? self.firstName : self.secondName);
+					($(this).html().split(" (@")[0] == self.firstName ? self.createAnswer("right") : self.createAnswer("wrong"));
+					$('.answer:first-child').append(' follows ' + self.firstFriends + ' people');
+					$('.answer:last-child').append(' follows ' + self.secondFriends + ' people');
+					break;
+				case 'statuses_count':
+					(self.firstStatuses > self.secondStatuses ? self.firstName : self.secondName);
+					($(this).html().split(" (@")[0] == self.firstName ? self.createAnswer("right") : self.createAnswer("wrong"));
+					$('.answer:first-child').append(' has ' + self.firstFriends + ' tweets');
+					$('.answer:last-child').append(' has ' + self.secondFriends + ' tweets');
+					break;
+				case 'creation_date':
+					(Date.parse(self.firstCreationDate) > Date.parse(self.secondCreationDate) ? self.firstName : self.secondName);
+					($(this).html().split(" (@")[0] == self.firstName ? self.createAnswer("right") : self.createAnswer("wrong"));
+					$('.answer:first-child').append(' has been on Twitter since ' + self.firstCreationDate.split(' ')[0]);
+					$('.answer:last-child').append(' has been on Twitter since ' + self.secondCreationDate.split(' ')[0]);
+					break;
+				default: 
+					console.log('error in checkAnswer!');
 			}
 		});
-	});
+	},
+	createAnswer: function(result){
+		var userId = $('.user').attr('id')
+		var data = {
+			'user_id': userId,
+			'question_id': this.questionId
+		}
+		if (result == 'right') {
+			data["is_correct?"] = true 
+		}
+		else {
+			data["is_correct?"] = false
+		}
+		$.ajax({
+			type: "POST",
+			dataType: "json",
+			data: data,
+			url: window.location.origin + "/questions/" + this.questionId + "/create_" + result + "_answer",
+			success: function(response, status, jqXHR) {
+				console.log(response);
+				$('#next').show();
+				$('#next').unbind('click');
+				$('#next').on('click', function(){
+					$(this).hide();
+					q = new Question();
+					q.load(function(response){
+						newView = new QuestionView(response);
+					});
+				});
+			},
+			error: function(response){
+				console.log("ERROR")
+			}
+		});
+		$('.question').html(result + '!');
+	}
 }
+
+var q = new Question();
+q.load(function(response){
+	var newView = new QuestionView(response);
+});
+
+
+// function updateScoreboard(){
+// 	$(".close").on("click", function(){
+// 		$.ajax({
+// 			type: "GET",
+// 			dataType: "json",
+// 			url: window.location.origin + "/users.json",
+// 			success: function(response){
+// 				$(".scoreboard").children().remove()
+// 				for(var i = 0; i < response.length; i++){
+// 					$(".scoreboard").append("<li><p>" + response[i]["email"]+ "</p><p>Score: " + response[i]["score"] + "</p><div class='bar-wrap'><span class='bar-fill' style='width: " + (response[i]["score"]/response[0]["score"]) * 100 + "%;'></span></div></li>");
+// 				}
+// 			},
+// 			error: function(response){
+// 				console.log("error");
+// 			}
+// 		});
+// 	});
+// }
 
