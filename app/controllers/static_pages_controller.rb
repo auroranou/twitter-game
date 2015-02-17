92134c6f51d4cbd0d3db4f5f774de9ecae38634d
Andy <andrew.sunglae.kim@gmail.com>
2015-02-16 21:33:45 -0500

    add top 5 scoreboard to main page, add update functionality to scoreboard on answer, tookout 80vh on container div

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 	# before_action :authenticate_user!
 
 	def index
-		@users = User.all.sort_by{ |user| user.score }.reverse
+		@users = User.all.sort_by{ |user| user.score }.reverse[0,5]
 		@topscore = @users[0]
 	end
 end
