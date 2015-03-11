db5546351d73ea0e5d4ef8254cde0fdfa061512b
Aurora Nou <auroranou@gmail.com>
2015-03-11 17:57:14 -0400

    add favicon

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128151512) do
+ActiveRecord::Schema.define(version: 20150308130916) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20150128151512) do
     t.integer "user_id"
     t.integer "question_id"
     t.boolean "is_correct?"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "company"
+    t.string "email"
+    t.string "message"
   end
 
   create_table "groups", force: :cascade do |t|
